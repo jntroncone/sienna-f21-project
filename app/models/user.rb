ca0ccf961d0089b3fbf26cf7acb8d6b2dff841f2
Julie Troncone <jntroncone@gmail.com>
2021-12-01 14:56:49 -0600

    user avatar v1

--- app/models/user.rb
+++ app/models/user.rb
@@ -20,7 +20,7 @@
 #
 class User < ApplicationRecord
   has_many :posts, dependent: :destroy
-
+  has_one_attached :avatar
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :sent_conversations, class_name: 'Conversation', foreign_key: 'sender_id'
